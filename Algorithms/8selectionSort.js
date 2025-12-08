24c6f5e02b3ed01217a301c0309f7d07246a38f3
chethazz <chethaskp10@gmail.com>
2025-12-08 10:58:56 +0530

    selection sort

--- Algorithms/8selectionSort.js
+++ Algorithms/8selectionSort.js
@@ -4,18 +4,20 @@
 
 // Big O(n^2)
 function selectionSort(arr) {
-    for(let i=0; i<arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
         let lowestIndex = i;
-        for(let j=i+1; j<arr.length; j++) {
-            if(arr[j] < arr[lowestIndex]) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[lowestIndex]) {
                 lowestIndex = j;
             }
         }
-
-        [arr[i], arr[lowestIndex]] = [arr[lowestIndex], arr[i]]
+        let temp = arr[i];
+        arr[i] = arr[lowestIndex];
+        arr[lowestIndex] = temp;
     }
-    return arr
+
+    return arr;
 }
 
-const arr = [ 7, 12, 9, 11, 3, 3];
-console.log(selectionSort(arr))
+const arr = [7, 12, 9, 11, 3, 3];
+console.log(selectionSort(arr));
