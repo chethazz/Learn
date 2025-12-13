7395c72a0f0d9e7feb86453c28a0546a0639f2e7
chethazz <chethaskp10@gmail.com>
2025-12-13 12:57:36 +0530

    alt rhombi/rhombuses & pyramid improvements

--- Patterns/alternativePatterns.js
+++ Patterns/alternativePatterns.js
@@ -1,13 +1,15 @@
 function fullPyramid(rows) {
     const cols = rows * 2 - 1;
-    const mid = rows - 1;
 
     for (let i = 0; i < rows; i++) {
         let row = "";
         for (let j = 0; j < cols; j++) {
             // Or j >= mid -i && j <= mid + i
+            if (j > rows - 1 + i) {
+                break;
+            }
             const l = j < rows ? j : cols - 1 - j;
-            if (i + l >= mid) {
+            if (i + l >= rows - 1) {
                 row += "* ";
             } else {
                 row += "  ";
@@ -23,10 +25,53 @@ function invertedFullPyramid(rows) {
     for (let i = 0; i < rows; i++) {
         let row = "";
         for (let j = 0; j < cols; j++) {
+            if (i + j >= cols) {
+                break;
+            }
             const l = j < rows ? j : cols - 1 - j;
             if (l >= i) {
                 row += "* ";
             } else if (l < i) {
+                row += "  ";
+            }
+        }
+        console.log(row);
+    }
+}
+
+invertedFullPyramid(5);
+
+function rightLeaningRhombus(rows) {
+    const cols = rows * 2 - 1;
+
+    for (let i = 0; i < rows; i++) {
+        let row = "";
+        for (let j = 0; j < cols; j++) {
+            if (i + j >= cols) {
+                break;
+            }
+            if (i + j < rows - 1) {
+                row += "  ";
+            } else {
+                row += "* ";
+            }
+        }
+        console.log(row);
+    }
+}
+
+function leftLeaningRhombus(rows) {
+    const cols = rows * 2 - 1;
+
+    for (let i = 0; i < rows; i++) {
+        let row = "";
+        for (let j = 0; j < cols; j++) {
+            if (j > rows - 1 + i) {
+                break;
+            }
+            if (j >= i) {
+                row += "* ";
+            } else {
                 row += "  ";
             }
         }
