31768a3fb9bc53298a1d12a2b167cc7675c68f40
chethazz <chethaskp10@gmail.com>
2025-12-14 10:46:57 +0530

    matrix diamong and hourglass

---
+++ Patterns/Matrix/diamongAndHourGlass.js
@@ -0,0 +1,39 @@
+function diamond(rows) {
+    const size = rows * 2 - 1;
+    for (let i = 0; i < size; i++) {
+        const l = i < rows ? i : size - 1 - i;
+        let row = "";
+        for (let j = 0; j < size; j++) {
+            const m = j < rows ? j : size - 1 - j;
+            if (l + m >= rows - 1) {
+                row += "* ";
+            } else {
+                row += "  ";
+            }
+            if (j - l === rows - 1) {
+                break;
+            }
+        }
+        console.log(row);
+    }
+}
+
+function hourGlass(rows) {
+    const size = rows * 2 - 1;
+    for (let i = 0; i < size; i++) {
+        const l = i < rows ? i : size - 1 - i;
+        let row = "";
+        for (let j = 0; j < size; j++) {
+            const m = j < rows ? j : size - 1 - j;
+            if (m >= l) {
+                row += "* ";
+            } else {
+                row += "  ";
+            }
+            if (l + j === size - 1) {
+                break;
+            }
+        }
+        console.log(row);
+    }
+}
