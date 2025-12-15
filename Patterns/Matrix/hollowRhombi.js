615c48beac27a2b0e960ef6fa834009641176021
chethazz <chethaskp10@gmail.com>
2025-12-15 10:42:34 +0530

    matrix: fix: rename to hollowRhombi, add rhombi

--- Patterns/Matrix/hollowRhombi.js
+++ Patterns/Matrix/hollowRhombi.js
@@ -1,13 +1,19 @@
-function rightLeaningRhombus(rows) {
+function hollowRightLeaningRhombus(rows) {
     const cols = rows * 2 - 1;
     for (let i = 0; i < rows; i++) {
         let row = "";
         for (let j = 0; j < cols; j++) {
-            if (i + j >= rows - 1 && i + j <= cols - 1) {
+            if (
+                i + j === rows - 1 ||
+                (i === 0 && j >= rows) ||
+                (i === rows - 1 && j < rows) ||
+                i + j === cols - 1
+            ) {
                 row += "* ";
             } else {
                 row += "  ";
             }
+
             if (i + j === cols - 1) {
                 break;
             }
@@ -16,12 +22,18 @@ function rightLeaningRhombus(rows) {
     }
 }
 
-function leftLeaningRhombus(rows) {
+function hollowLeftLeaningRhombus(rows) {
     const cols = rows * 2 - 1;
     for (let i = 0; i < rows; i++) {
         let row = "";
         for (let j = 0; j < cols; j++) {
-            if (j >= i && j - i <= rows - 1) {
+            const l = j < rows ? j : cols - 1 - j;
+            if (
+                i === j ||
+                j - i === rows - 1 ||
+                (i === rows - 1 && j >= rows) ||
+                (i === 0 && j < rows)
+            ) {
                 row += "* ";
             } else {
                 row += "  ";
