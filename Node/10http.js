c349950a3cabf246ad7133fa5534e0549aa4eabc
chethazz <chethaskp10@gmail.com>
2026-01-04 12:15:09 +0530

    trailing commas

--- Node/10http.js
+++ Node/10http.js
@@ -1,11 +1,12 @@
 const http = require("node:http");
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Hello world!");
+	res.statusCode = 200;
+	res.setHeader("Content-Type", "text/plain");
+	res.end("Hello world!");
 });
 
 server.listen(3000, () => {
-    console.log("Server running on port 3000");
+	console.log("Server running on port 3000");
 });
+const=
--- Node/11jsonResponse.js
+++ Node/11jsonResponse.js
@@ -1,16 +1,16 @@
 const http = require("node:http");
 
 const superHero = {
-    firstName: "Bruce",
-    lastName: "Wayne"
+	firstName: "Bruce",
+	lastName: "Wayne",
 };
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify(superHero));
+	res.statusCode = 200;
+	res.setHeader("Content-Type", "application/json");
+	res.end(JSON.stringify(superHero));
 });
 
 server.listen(3000, () => {
-    console.log("Server running on port 3000");
+	console.log("Server running on port 3000");
 });
