c09f6fa85f27e029983f23796aff16860668de67
chethazz <chethaskp10@gmail.com>
2026-01-04 12:37:27 +0530

    html response

---
+++ Node/12htmlResponse.js
@@ -0,0 +1,17 @@
+const http = require("node:http");
+const fs = require("node:fs");
+const path = require("node:path");
+
+const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader("Content-Type", "text/html");
+	// const html = fs.readFileSync(__dirname + "/12htmlResponse.html", "utf-8");
+	// res.end(html);
+
+	// Better performance
+	fs.createReadStream(__dirname + "/12htmlResponse.html", "utf-8").pipe(res);
+});
+
+server.listen(3000, () => {
+	console.log("Server running on port 3000");
+});
