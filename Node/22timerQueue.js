da2bfba7883cfbf6f0aa1222b6d37dec95db6c3a
chethazz <chethaskp10@gmail.com>
2026-01-05 12:42:48 +0530

    timer queue: add bp

--- Node/22timerQueue.js
+++ Node/22timerQueue.js
@@ -1,3 +1,5 @@
+// IMP: Timer queue isn't exactly a queue, but rather a min heap
+
 setTimeout(() => console.log("This is setTimeout 1"), 1000);
 setTimeout(() => console.log("This is setTimeout 2"), 500);
 setTimeout(() => console.log("This is setTimeout 3"), 0);
