32292ff7f4114f553b6ccadbaca682e614c6ba15
chethazz <chethaskp10@gmail.com>
2026-01-08 16:32:26 +0530

    path: more readable output

--- Node/2path.js
+++ Node/2path.js
@@ -1,18 +1,21 @@
 const path = require("node:path");
 
-console.log(path.join("folder1", "folder2", "index.html"));
-console.log(path.join("/folder1", "folder2", "index.html"));
+console.log("Join");
+// Uses platform specific separator
+console.log(1, path.join("folder1", "folder2", "index.html"));
+console.log(2, path.join("/folder1", "folder2", "index.html"));
 
 // Also normalizes
-console.log(path.join("folder1", "//folder2", "index.html"));
-console.log(path.join("folder1", "folder2", "../index.html"));
-console.log(path.join(__dirname, "data.json"));
+console.log(3, path.join("folder1", "//folder2", "index.html"));
+console.log(4, path.join("folder1", "folder2", "../index.html"));
+console.log(5, path.join(__dirname, "data.json"));
 
+console.log("Resolve");
 // Resolves to absolute path
-console.log(path.resolve("folder1", "folder2", "index.html"));
-console.log(path.resolve("/folder1", "folder2", "index.html"));
+console.log(6, path.resolve("folder1", "folder2", "index.html"));
+console.log(7, path.resolve("/folder1", "folder2", "index.html"));
 
 // Also normalizes and if it sees / later, it considers that as the root
-console.log(path.resolve("/folder1", "//folder2", "index.html"));
-console.log(path.resolve("/folder1", "//folder2", "../index.html"));
-console.log(path.resolve(__dirname, "data.json"));
+console.log(8, path.resolve("/folder1", "//folder2", "index.html"));
+console.log(9, path.resolve("/folder1", "//folder2", "../index.html"));
+console.log(10, path.resolve(__dirname, "data.json"));
