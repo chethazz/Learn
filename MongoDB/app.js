4b7c1b5d40ac6ff5dee2ab570e140d40005a847f
chethazz <chethaskp10@gmail.com>
2026-01-17 17:39:19 +0530

    add and delete books

--- MongoDB/app.js
+++ MongoDB/app.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { MongoClient, ObjectId } from "mongodb";
+import { ObjectId } from "mongodb";
 import { connectToDb, getDb } from "./db.js";
 
 const app = express();
+app.use(express.json());
 
 app.get("/books", (req, res) => {
 	const db = getDb();
@@ -27,6 +28,40 @@ app.get("/books/:id", (req, res) => {
 		const db = getDb();
 		db.collection("books")
 			.findOne({ _id: new ObjectId(req.params.id) })
+			.then((book) => {
+				if (book) {
+					res.json(book);
+				} else {
+					res.status(404).json({ message: "Book not found" });
+				}
+			})
+			.catch(() => {
+				res.status(500).json({ error: "Internal server error" });
+			});
+	} else {
+		res.status(400).json({ message: "Enter a valid id" });
+	}
+});
+
+app.post("/books", (req, res) => {
+	const db = getDb();
+
+	db.collection("books")
+		.insertOne(req.body)
+		.then((result) => {
+			res.status(201).json(result);
+		})
+		.catch((err) => {
+			res.status(500).json({ error: "Internal server error" });
+		});
+});
+
+app.delete("/books/:id", (req, res) => {
+	if (ObjectId.isValid(req.params.id)) {
+		const db = getDb();
+
+		db.collection("books")
+			.deleteOne({ _id: new ObjectId(req.params.id) })
 			.then((book) => {
 				if (book) {
 					res.json(book);
